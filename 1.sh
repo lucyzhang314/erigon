8f857b12131df1261b5fa51dc889ee880e9b924e
alex.sharov <AskAlexSharov@gmail.com>
2024-09-27 14:29:46 +0200

    save

--- 1.sh
+++ 1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo $1
 
-go run ./cmd/erigon snapshots uncompress /erigon-data/snapshots_v0/$1 | DictReducerSoftLimit=1000000 MinPatternLen=5 MaxPatternLen=128 SamplingFactor=1 MaxDictPatterns=65536 go run ./cmd/erigon snapshots compress --datadir=/erigon-data/erigon3/ /erigon-data/snapshots/$1 > ~/log3.txt 2>&1 &
+go run ./cmd/erigon snapshots uncompress /erigon-data/snapshots_v0/$1 | DictReducerSoftLimit=1000000 MinPatternLen=5 MaxPatternLen=128 SamplingFactor=1 MaxDictPatterns=65536 go run ./cmd/erigon snapshots compress --datadir=/erigon-data/erigon3/ /erigon-data/snapshots/$1 > ~/log3.txt 2>&1
 
+echo "$1 done"
