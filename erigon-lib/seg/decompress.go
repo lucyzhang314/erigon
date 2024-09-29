bf83bbbbad0d68309b5d9e9c2b95715620ac0538
alex.sharov <AskAlexSharov@gmail.com>
2024-09-29 12:01:06 +0200

    save

--- erigon-lib/seg/decompress.go
+++ erigon-lib/seg/decompress.go
@@ -276,11 +276,12 @@ func NewDecompressor(compressedFilePath string) (*Decompressor, error) {
 		if _, err = buildCondensedPatternTable(d.dict, depths, patterns, 0, 0, 0, patternMaxDepth); err != nil {
 			return nil, &ErrCompressedFileCorrupted{FileName: fName, Reason: err.Error()}
 		}
-		//var dep uint64
-		//for i := range depths {
-		//	dep += depths[i]
-		//}
-		//fmt.Printf("depths=%d, %d, %s, \n", dep, len(depths), fName)
+		var dep uint64
+		for i := range depths {
+			dep += depths[i]
+		}
+		log.Warn(fmt.Sprintf("[dbg] depths=%d, %d, %s", dep, len(depths), fName))
+
 	}
 
 	if assert.Enable && pos != 24 {
