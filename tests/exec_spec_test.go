78c014ae972ad47a1fd8f60dcd6e5fa7f39a9f36
yperbasis <andrey.ashikhmin@gmail.com>
2024-10-04 12:33:09 +0200

    debug test

--- tests/exec_spec_test.go
+++ tests/exec_spec_test.go
@@ -1,5 +1,3 @@
-//go:build integration
-
 package tests
 
 import (
@@ -20,15 +18,11 @@ func TestExecutionSpec(t *testing.T) {
 
 	bt := new(testMatcher)
 
-	dir := filepath.Join(".", "execution-spec-tests")
+	path := filepath.Join(".", "execution-spec-tests", "prague", "eip2537_bls_12_381_precompiles", "bls12_g1add", "valid.json")
 
-	// TODO(yperbasis) make it work
-	bt.skipLoad(`^prague/eip2935_historical_block_hashes_from_state/block_hashes/block_hashes_history.json`)
-	bt.skipLoad(`^prague/eip7251_consolidations/`)
-	bt.skipLoad(`^prague/eip7685_general_purpose_el_requests/`)
-	checkStateRoot := true
+	checkStateRoot := false
 
-	bt.walk(t, dir, func(t *testing.T, name string, test *BlockTest) {
+	bt.runTestFile(t, path, "", func(t *testing.T, name string, test *BlockTest) {
 		// import pre accounts & construct test genesis block & state root
 		if err := bt.checkFailure(t, test.Run(t, checkStateRoot)); err != nil {
 			t.Error(err)
