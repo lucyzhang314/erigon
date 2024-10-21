e656f2e4de35d0d001c9966ac921ac51f9b2f35f
kewei <kewei.train@gmail.com>
2024-10-22 04:17:31 +0800

    update

--- cl/transition/impl/eth2/statechange/process_registry_updates.go
+++ cl/transition/impl/eth2/statechange/process_registry_updates.go
@@ -49,10 +49,8 @@ func ProcessRegistryUpdates(s abstract.BeaconState) error {
 	// Process activation eligibility and ejections.
 	if err := threading.ParallellForLoop(runtime.NumCPU(), 0, s.ValidatorSet().Length(), func(i int) error {
 		validator := s.ValidatorSet().Get(i)
-		activationEligibilityEpoch := validator.ActivationEligibilityEpoch()
 		effectivaBalance := validator.EffectiveBalance()
-		if activationEligibilityEpoch == s.BeaconConfig().FarFutureEpoch &&
-			validator.EffectiveBalance() == s.BeaconConfig().MaxEffectiveBalance {
+		if state.IsValidatorEligibleForActivationQueue(s, validator) {
 			s.SetActivationEligibilityEpochForValidatorAtIndex(i, currentEpoch+1)
 		}
 		if validator.Active(currentEpoch) && effectivaBalance <= beaconConfig.EjectionBalance {
@@ -61,6 +59,7 @@ func ProcessRegistryUpdates(s abstract.BeaconState) error {
 			}
 		}
 		// Insert in the activation queue in case.
+		activationEligibilityEpoch := validator.ActivationEligibilityEpoch()
 		if activationEligibilityEpoch <= s.FinalizedCheckpoint().Epoch &&
 			validator.ActivationEpoch() == s.BeaconConfig().FarFutureEpoch {
 			m.Lock()
