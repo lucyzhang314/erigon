9b029e2ba039fc4b2f23086aa248fd983a350d8e
steven <wangpeculiar@gmail.com>
2024-10-23 19:40:52 +0800

    use rotx when prune

--- turbo/snapshotsync/freezeblocks/block_snapshots.go
+++ turbo/snapshotsync/freezeblocks/block_snapshots.go
@@ -1056,6 +1056,8 @@ func (s *RoSnapshots) Delete(fileName string) error {
 	if s == nil {
 		return nil
 	}
+	v := s.View()
+	defer v.Close()
 	defer s.recalcVisibleFiles()
 	if err := s.delete(fileName); err != nil {
 		return fmt.Errorf("can't delete file: %w", err)
