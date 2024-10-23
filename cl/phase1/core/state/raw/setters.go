55af820c0581e5a611d5b0175e5cca4f47fe3d20
kewei <kewei.train@gmail.com>
2024-10-24 02:51:38 +0800

    update

--- cl/phase1/core/state/raw/setters.go
+++ cl/phase1/core/state/raw/setters.go
@@ -528,3 +528,8 @@ func (b *BeaconState) SetPendingPartialWithdrawals(pendingWithdrawals *solid.Lis
 	b.pendingPartialWithdrawals = pendingWithdrawals
 	b.markLeaf(PendingPartialWithdrawalsLeafIndex)
 }
+
+func (b *BeaconState) AppendPendingDeposit(deposit *solid.PendingDeposit) {
+	b.pendingDeposits.Append(deposit)
+	b.markLeaf(PendingDepositsLeafIndex)
+}
